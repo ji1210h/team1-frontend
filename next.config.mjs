f417f143377a2eadbbede8da505bd2124b970d7c
leejihee <jihee6986@naver.com>
2024-05-30 16:44:22 +0900

    9

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "(전달받은 API 주소)/:path*",
-      },
-    ];
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
-// export default nextConfig;
+export default nextConfig;
 

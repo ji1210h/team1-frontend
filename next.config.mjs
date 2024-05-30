87a8b20eaab621cc7d54959d4f70574d0c528552
leejihee <jihee6986@naver.com>
2024-05-30 16:34:07 +0900

    7

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,18 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    async rewrites() {
-      return [
-        {
-          source: "/:path*",
-          destination: "http://54.180.2.181:3000/:path*",
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://54.180.2.181:3000/:path*",
+      },
+    ];
+  },
+};
+
+// module.exports = nextConfig;
+
+export default nextConfig;
+
